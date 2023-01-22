ba85ae34cc7c53c909683eafaa119834e8f7fab5
yuli.zhang <2285546178@qq.com>
2023-01-22 20:40:25 +0800

    currifyWithHoles

--- currify.js
+++ currify.js
@@ -1,21 +1,7 @@
-function sayHello(name, age, fruit) {
-  console.log(console.log(`我叫 ${name},我 ${age} 岁了, 我喜欢吃 ${fruit}`))
-}
-
-const curryingShowMsg1 = currying(sayHello, '小明')
-console.log(222, curryingShowMsg1);
-curryingShowMsg1(22, '苹果')            // 我叫 小明,我 22 岁了, 我喜欢吃 苹果
-
-const curryingShowMsg2 = currying(sayHello, '小衰', 20)
-curryingShowMsg2('西瓜')               // 我叫 小衰,我 20 岁了, 我喜欢吃 西瓜
-
-
-const curryingShowMsg3 = currying(sayHello)
-curryingShowMsg3('haha')(20)('banana')
-
 
 function currying(fn, ...args1) {
   // args没有...的话只能拿到第一个参数
+  // 这里的this 是undefined
   const allArgs = [...args1];
   const next = (...args2) => {
     allArgs.push(...args2);
@@ -24,3 +10,60 @@ function currying(fn, ...args1) {
   return next
 }
 
+
+// 需要约定一下？ '_'为占位符，
+function currifyWithHoles(fn, ...args1) {
+  // args没有...的话只能拿到第一个参数
+  // 这里的this 是undefined
+  let allArgs = [];
+  let holes = []; // 存储占位符在的位置
+  function checkArgs(args) {
+    args.length && args.forEach((arg, index) => {
+      if (!holes.length && arg !== '_') {
+        allArgs.push(arg);
+      } else {
+        if (arg === '_') {
+          holes.push(index)
+        } else {
+          const minHole = holes.shift();
+          // 避免了在遍历自己的时候给之前的hole搞没了
+          if (minHole >= index) {
+            allArgs.splice(minHole, 0, arg);
+          } else {
+            allArgs.push(arg);
+            holes.unshift(minHole)
+          }
+        }
+      }
+    })
+  }
+  checkArgs(args1);
+  const next = (...args2) => {
+    checkArgs(args2);
+    if (allArgs.length >= fn.length) {
+      const res = fn.apply(undefined, allArgs);
+      allArgs = []
+      holes = [];
+      return res
+    } else {
+      return next
+    }
+  }
+  return next
+}
+
+
+
+/** 测试代码 **/
+function sayHello(name, age, fruit) {
+  console.log(`我叫 ${name},我 ${age} 岁了, 我喜欢吃 ${fruit}`)
+}
+
+const showMsg = currying(sayHello);
+showMsg('小衰', 20)('西瓜')
+
+const betterShowMsg = currifyWithHoles(sayHello)
+betterShowMsg('_', 20)('小衰', '_', '西瓜')          // 我叫 小衰,我 20 岁了, 我喜欢吃 西瓜
+betterShowMsg('_', '_', '南瓜')('小猪')(25)          // 我叫 小猪,我 25 岁了, 我喜欢吃 南瓜
+betterShowMsg('小明')('_', 22)('_', '_', '倭瓜')          // 我叫 小明,我 22 岁了, 我喜欢吃 倭瓜
+betterShowMsg('小拽')(28)('冬瓜')          // 我叫 小拽,我 28 岁了, 我喜欢吃 冬瓜
