62482198b0de296657f1191d409dbf67a3178c61
yuli.zhang <2285546178@qq.com>
2023-01-27 12:05:53 +0800

    add eventBus

--- eventBus.js
+++ eventBus.js
@@ -1,11 +1,30 @@
+// 任务队列
 const eventBus = {
-  queue: [],
-  on: function () {
-
+  queueMap: {},
+  on: function (name, fn) {
+    eventBus.queueMap[name] = eventBus.queueMap[name] || [];
+    eventBus.queueMap[name].push(fn)
   },
-  // 这里不能写肩头函数，因为拿不到里面的queue
-  off: () => {
-
+  off: (name, fn) => {
+    const index = eventBus.queueMap[name]?.findIndex(eve => eve === fn);
+    if (index !== -1 && index !== undefined) eventBus.queueMap[name].splice(index, 1)
   },
-  trigger: () => { }
+  emit: (name, data) => {
+    eventBus.queueMap[name]?.forEach(eve => {
+      eve?.call(undefined, data);
+    });
+  }
 }
+
+
+const fn = (arg) => {
+  console.log('clicked', arg);
+}
+eventBus.on('click', fn)
+setTimeout(() => {
+  eventBus.emit('click', 'test')
+  eventBus.off('click', fn);
+}, 1000)
+setTimeout(() => {
+  eventBus.emit('click', 'test2')
+}, 1000)
