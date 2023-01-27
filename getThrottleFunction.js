9a356684c830488db7387388dc9f5fb88e630976
yuli.zhang <2285546178@qq.com>
2023-01-27 11:30:20 +0800

    update throttle

--- getThrottleFunction.js
+++ getThrottleFunction.js
@@ -1,14 +1,14 @@
 
 //  技能冷却
 function getThrottleFunction(fn, time) {
-  let throttling = false;
   let timer = null;
   return (...args) => {
-    if (throttling) return
+    if (timer) return
     fn.call(undefined, ...args);
     throttling = true;
     timer = setTimeout(() => {
-      throttling = false;
+      timer = null;
     }, time)
   }
 }
+
