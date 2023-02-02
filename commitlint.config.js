d9f17142c3034b92581da044358e3663dfd100a6
yuli.zhang <2285546178@qq.com>
2023-02-02 11:50:05 +0800

    add

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,14 +1,66 @@
+/** @type {import('cz-git').UserConfig} */
+
 const {
-  types,
-  typesForRules,
-  typeEnumForQuestions
-} = require('./commitlint.config.type.js');
-const CONFIG_COMMON = require('./commitlint.config.common')
+  typeEnumForQuestions, typesForRules
+} = require('./xxxxx.js')
 
 module.exports = {
-  ...CONFIG_COMMON,
-  types,
+  alias: { fd: 'docs: fix typos' },
+  messages: {
+    type: 'Select the type of change that you\'re committing:',
+    scope: 'Denote the SCOPE of this change (optional):',
+    customScope: 'Denote the SCOPE of this change:',
+    subject: 'Write a SHORT, IMPERATIVE tense description of the change:\n',
+    body: 'Provide a LONGER description of the change (optional). Use "|" to break new line:\n',
+    breaking: 'List any BREAKING CHANGES (optional). Use "|" to break new line:\n',
+    footerPrefixesSelect: 'Select the ISSUES type of changeList by this change (optional):',
+    customFooterPrefix: 'Input ISSUES prefix:',
+    footer: 'List any ISSUES by this change. E.g.: #31, #34:\n',
+    confirmCommit: 'Are you sure you want to proceed with the commit above?'
+  },
+  types: [
+    { value: "feat", name: "feat:     ✨  A new feature", emoji: ":sparkles:" },
+    { value: "fix", name: "fix:      🐛  A bug fix", emoji: ":bug:" },
+    { value: "docs", name: "docs:     📝  Documentation only changes", emoji: ":memo:" },
+    { value: "style", name: "style:    💄  Changes that do not affect the meaning of the code", emoji: ":lipstick:" },
+    { value: "refactor", name: "refactor: ♻️   A code change that neither fixes a bug nor adds a feature", emoji: ":recycle:" },
+    { value: "perf", name: "perf:     ⚡️  A code change that improves performance", emoji: ":zap:" },
+    { value: "test", name: "test:     ✅  Adding missing tests or correcting existing tests", emoji: ":white_check_mark:" },
+    { value: "build", name: "build:    📦️   Changes that affect the build system or external dependencies", emoji: ":package:" },
+    { value: "ci", name: "ci:       🎡  Changes to our CI configuration files and scripts", emoji: ":ferris_wheel:" },
+    { value: "chore", name: "chore:    🔨  Other changes that don't modify src or test files", emoji: ":hammer:" },
+    { value: "revert", name: "revert:   ⏪️  Reverts a previous commit", emoji: ":rewind:" }
+  ],
+  // 下面这个可能是为了校验用的 emoji好像还是得自己手动加 难受
+  useEmoji: true,
+  emojiAlign: 'center',
+  themeColorCode: '',
+  // 结合commitlint 可以在这里做更多的配置 scopes的范围
   scopes: [],
+  allowEmptyScopes: false,
+  customScopesAlign: 'bottom',
+  customScopesAlias: 'custom',
+  emptyScopesAlias: 'empty',
+  markBreakingChangeMode: false,
+  breaklineNumber: 100,
+  breaklineChar: '|',
+  skipQuestions: ['breaking', 'footerPrefix', 'footer', 'confirmCommit'],
+  issuePrefixes: [{ value: 'closed', name: 'closed:   ISSUES has been processed' }],
+  customIssuePrefixAlign: 'top',
+  emptyIssuePrefixAlias: 'skip',
+  customIssuePrefixAlias: 'custom',
+  allowCustomIssuePrefix: true,
+  allowEmptyIssuePrefix: true,
+  confirmColorize: true,
+  maxHeaderLength: Infinity,
+  maxSubjectLength: Infinity,
+  minSubjectLength: 0,
+  scopeOverrides: undefined,
+  defaultBody: '',
+  defaultIssues: '',
+  defaultScope: '',
+  defaultSubject: '',
+
   upperCaseSubject: true,
   footerPrefix: "Related issue:",
   allowCustomScopes: true,
@@ -35,7 +87,6 @@ module.exports = {
       typesForRules
     ]
   },
-  // 12版本的@commitlint/load拿不到prompt
   prompt: {
     settings: {},
     messages: {
@@ -60,4 +111,4 @@ module.exports = {
       }
     }
   }
-};
+}
