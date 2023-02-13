e51e9412911100430345d8845642b0cbb362eed5
yuli.zhang <2285546178@qq.com>
2023-02-13 22:07:30 +0800

    feat(debounce): update

--- getDebounceFunction.js
+++ getDebounceFunction.js
@@ -1,10 +1,42 @@
 function getDebounceFunction(fn, time) {
   let timer = null;
-  return (...args) => {
+  const debounce = (...args) => {
     if (timer) clearTimeout(timer);
     timer = setTimeout(() => {
       fn.call(undefined, ...args);
       timer = null;
-    }, time)
-  }
+    }, time);
+  };
+  return debounce;
+}
+
+// 第四版 immediate增加了立即执行的功能
+function debounce(func, wait, immediate) {
+  var timeout;
+
+  const debounce = function () {
+    var context = this;
+    var args = arguments;
+
+    if (timeout) clearTimeout(timeout);
+    if (immediate) {
+      // 如果已经执行过，不再执行
+      var callNow = !timeout;
+      timeout = setTimeout(function () {
+        timeout = null;
+      }, wait);
+      if (callNow) func.apply(context, args);
+    } else {
+      timeout = setTimeout(function () {
+        func.apply(context, args);
+      }, wait);
+    }
+  };
+
+  // 增加取消功能，执行后可以取消当前防抖
+  debounce.cancel = () => {
+    if (timeout) clearTimeout(timer);
+    timeout = null;
+  };
+  return debounce;
 }
--- getThrottleFunction.js
+++ getThrottleFunction.js
@@ -1,14 +1,11 @@
-
 //  技能冷却
 function getThrottleFunction(fn, time) {
   let timer = null;
   return (...args) => {
-    if (timer) return
+    if (timer) return;
     fn.call(undefined, ...args);
-    throttling = true;
     timer = setTimeout(() => {
       timer = null;
-    }, time)
-  }
+    }, time);
+  };
 }
-
