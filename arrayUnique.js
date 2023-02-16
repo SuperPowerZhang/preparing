b0c6a237eacd24a936563c66ee4a623df9cddaa3
yuli.zhang <2285546178@qq.com>
2023-02-16 22:19:04 +0800

    feat(array): update unique

--- arrayUnique.js
+++ arrayUnique.js
@@ -1,7 +1,10 @@
+// Array.indexOf 底层还是使用 === 进行判断，因为 NaN === NaN的结果为 false，所以使用 indexOf 查找不到 NaN 元素
+// Set 认为尽管 NaN === NaN 为 false，但是这两个元素是重复的
+
 // set
 function unique1(arr) {
-  return Array.from(new Set(arr))
-  // [...new Set(arr)] 
+  return Array.from(new Set(arr));
+  // [...new Set(arr)]
 }
 
 // 计数排序 与下面的Map类似，但是直接用object，缺点是key是字符串导致一些数据类型的结果不对
@@ -11,12 +14,36 @@ function unique1(arr) {
 function unique3(arr) {
   const arrMap = new Map();
   for (let i = 0; i < arr.length; i++) {
-    if (arr[i] === undefined) { continue }
+    if (arr[i] === undefined) {
+      continue;
+    }
     if (!arrMap.has(arr[i])) {
       arrMap.set(arr[i], true);
     }
   }
-  return [...arrMap.keys()]
+  return [...arrMap.keys()];
+}
+
+// 排序后去重与上一个比较就可以了
+function unique4(array) {
+  var res = [];
+  var sortedArray = array.concat().sort();
+  var seen;
+  for (var i = 0, len = sortedArray.length; i < len; i++) {
+    // 如果是第一个元素或者相邻的元素不相同
+    if (!i || seen !== sortedArray[i]) {
+      res.push(sortedArray[i]);
+    }
+    seen = sortedArray[i];
+  }
+  return res;
+}
+
+function unique5(array) {
+  var res = array.filter(function (item, index, array) {
+    return array.indexOf(item) === index;
+  });
+  return res;
 }
 
 const a = [1, '2', 2, 3, 3, 2, 5];
