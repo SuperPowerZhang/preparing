24e38e3b14960b130d2c615c7f99625ba08fdad9
yuli.zhang <2285546178@qq.com>
2023-02-16 22:11:12 +0800

    feat(clone): uodate

--- deepClone.js
+++ deepClone.js
@@ -10,7 +10,7 @@
 //   obj: { name: 'haha', age: 18 }
 // }
 function deepClone1(data) {
-  return JSON.parse(JSON.stringify(data))
+  return JSON.parse(JSON.stringify(data));
 }
 
 // 判断是箭头函数还是普通函数
@@ -23,12 +23,12 @@ const deepClone2 = (data, cache) => {
     if (data instanceof Function) {
       if (data.prototype) {
         cloneData = function () {
-          return data.call(this, arguments)
-        }
+          return data.call(this, arguments);
+        };
       } else {
         cloneData = (...args) => {
-          return data.call(undefined, ...args)
-        }
+          return data.call(undefined, ...args);
+        };
       }
     } else if (data instanceof Array) {
       cloneData = [];
@@ -39,19 +39,23 @@ const deepClone2 = (data, cache) => {
     } else {
       cloneData = {};
     }
-    cache.set(data, cloneData)
+    cache.set(data, cloneData);
     for (let key in data) {
       // 注意继承属性
       if (data.hasOwnProperty(key)) {
-        cloneData[key] = deepClone2(data[key], cache)
+        cloneData[key] = deepClone2(data[key], cache);
       }
     }
     return cloneData;
+    // Object.prototype.toString.call(Symbol(111))
+    // typeof data === 'symbol'
+  } else if (Object.prototype.toString.call(data) === '[object Symbol]') {
+    cloneData = Object(Symbol.prototype.valueOf.call(origin));
   } else {
     cloneData = data;
     return cloneData;
   }
-}
+};
 
 const a = {
   number: 1,
@@ -59,16 +63,21 @@ const a = {
   str: 'hi',
   empty1: undefined,
   empty2: null,
-  array: [{ name: 'frank', age: 18 }, { name: 'jacky', age: 19 }],
+  array: [
+    { name: 'frank', age: 18 },
+    { name: 'jacky', age: 19 }
+  ],
   date: new Date(2000, 0, 1, 20, 30, 0),
   regex: /\.(j|t)sx/i,
   obj: { name: 'frank', age: 18 },
   f1: (a, b) => a + b,
-  f2: function (a, b) { return a + b }
-}
+  f2: function (a, b) {
+    return a + b;
+  }
+};
 
 // a.self = a
-const b = deepClone2(a)
+const b = deepClone2(a);
 b.obj.name = 'haha';
 console.log(b);
 console.log(a);
